c9239dbb260b429c3c4ffe51c7967331582158d1
K1tba <98889585+K1tba@users.noreply.github.com>
2026-06-01 17:17:43 +0300

    2 Реализовал функции: sortStrings, pick, omit

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -6,4 +6,29 @@
  */
 export const pick = (obj, ...fields) => {
 
+  /* Как я понимаю, в JS нет единого и надежного способа проверки
+  содержимого переменной на объект, поэтому нагородил каскад проверок. */
+  if (obj === null) {
+    return;
+  }
+
+  if (typeof obj !== 'object') {
+    return;
+  }
+
+  if (Array.isArray(obj)) {
+    return;
+  }
+  
+  /* Проверять fields, наверное смысла нет?
+  Так как чисто теоретически оно всегда массив */
+
+  let newObject = {};
+  for (let key of fields) {
+    if (key in obj) {
+      newObject[key] = obj[key];
+    }
+  }
+
+  return newObject;
 };
